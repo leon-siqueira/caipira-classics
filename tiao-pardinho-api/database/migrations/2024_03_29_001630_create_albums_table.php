6e61910c21c4a9fc0562cfeb0f5ec5f99c1f26ca
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-28 21:57:25 -0300

    rename

--- tiao-pardinho-api/database/migrations/2024_03_29_001630_create_albums_table.php
+++ tiao-pardinho-api/database/migrations/2024_03_29_001630_create_albums_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('album', function (Blueprint $table) {
+        Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->integer('year');
             $table->string('artist');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('album');
+        Schema::dropIfExists('albums');
     }
 };
