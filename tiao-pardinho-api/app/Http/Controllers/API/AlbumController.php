d5a3ebe4b50839caa0466922e857d71f72fa1148
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-28 22:36:52 -0300

    album controller

---
+++ tiao-pardinho-api/app/Http/Controllers/API/AlbumController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return Album::all();
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $album = Album::create($request->all());
+    return response()->json($album, 201);
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    $album = Album::findOrFail($id);
+    return response()->json($album, 200);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    $album = Album::findOrFail($id);
+    $album->delete();
+    return response()->json(null, 204);
+  }
+}
--- tiao-pardinho-api/routes/api.php
+++ tiao-pardinho-api/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AlbumController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('albums', AlbumController::class)->except('update');
