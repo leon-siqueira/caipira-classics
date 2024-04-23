d843de7eafadb1978b6e4df64988fb113d750716
Leon Siqueira <leon.siqueir4@gmail.com>
2024-04-22 22:50:51 -0300

    refactor: style improvements

--- tiao-pardinho-frontend/src/components/common/SearchBar/Suggestions/style.js
+++ tiao-pardinho-frontend/src/components/common/SearchBar/Suggestions/style.js
@@ -1,28 +1,46 @@
-
 export default function style() {
   return ({
     suggestionBox: {
       position: 'absolute',
       top: 56,
-      backgroundColor: '#FAFAFA',
+      backgroundColor: '#FFF',
       minWidth: '300px',
       color: '#000',
       borderRadius: '2px',
       zIndex: 1,
+      boxShadow: '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)'
     },
     suggestionList: {
-      paddingY: 0.2,
-      '& li:last-child div': {
+      paddingY: 0,
+      '& li:last-child': {
         borderBottom: 'none'
+      },
+      '& li:hover': {
+        backgroundColor: '#EEE',
+
+        transition: 'background-color 0.2s'
       }
+    },
+    listTitle: {
+      width: '100%',
+      backgroundColor: '#1976d2cc',
+      paddingTop: 0.8,
+      '& .MuiTypography-root': {
+        color: '#FFF',
+        padding: 0.5,
+        paddingLeft: 1,
+        fontSize: '0.75rem',
+      },
+
     },
     listItem: {
       cursor: 'pointer',
       paddingY: 0,
+      borderBottom: '1px solid #BBB',
       '& .MuiListItemText-root': {
-        borderBottom: '1px solid #CCC',
         margin: 0,
-        marginY: 0.25,
+        paddingX: 0,
+        paddingY: 0.25,
       }
     }
   })
